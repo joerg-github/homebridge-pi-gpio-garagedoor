48cd76a74437a72ad98428eca4f2ae6c6857317a
jmika <joerg.mika@mikatiming.de>
2020-10-12 21:12:25 +0200

    WIP

--- src/garageDoorControl.ts
+++ src/garageDoorControl.ts
@@ -16,14 +16,32 @@ enum TargetDoorState {
 
 export class GarageDoorControl {
 
-  currentDoorState: CurrentDoorState;
-  targetDoorState: TargetDoorState;
+  private _currentDoorState: CurrentDoorState = CurrentDoorState.CLOSED;
+  private _targetDoorState: TargetDoorState = TargetDoorState.CLOSED;
+
+  public get currentDoorState(): CurrentDoorState {
+    return this._currentDoorState;       
+  }
+
+  public set currentDoorState(DoorState: CurrentDoorState) {
+    this._currentDoorState = DoorState;
+  }
+
+  public get targetDoorState(): TargetDoorState {
+    return this._targetDoorState;
+  }
+
+  public set targetDoorState(value: TargetDoorState) {
+    this._targetDoorState = value;
+    
+    this.currentDoorState = value as number;
+  }
+
 
   constructor(log: Logging, config: GarageDoorConfig) {
 
     log.info('Initializing GarageDoorControl');
 
-    this.currentDoorState = CurrentDoorState.CLOSED;
     this.targetDoorState = TargetDoorState.CLOSED;
 
   }
